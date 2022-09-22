45cd83bf94b90713e0805dd213ca6396a4f478fc
Yoalfre <yoalfreparra@gmail.com>
2022-09-22 19:32:56 -0300

    segunda subida

---
+++ cac/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('hola_mundo', views.hola_mundo),
+    path('saludar/<str:nombre>', views.saludar),
+    path('proyectos/<int:anio>/<int:mes>',views.ver_proyectos,name="ver_proyectos" ),
+]
